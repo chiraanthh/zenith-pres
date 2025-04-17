cffcb080f8d245ff7a3cd8b081fafc141b922cdf
Chiraanth S <chiraanthcs.3055@gmail.com>
2025-04-17 09:42:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -310,6 +310,11 @@ def add_stock():
         return redirect(url_for('admin_dashboard') + '#admin-inventory')
 
 
-# ---------------- MAIN ----------------
+
+
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
